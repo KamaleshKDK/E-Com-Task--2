7547119c06bf998ba656cd83090a3eadbb64912d
kamaleshKDK <kamaleshkdk@gmail.com>
2022-06-26 12:23:19 +0530

    image changed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,11 +7,11 @@ function Header() {
  <div className="header">
         <div className="headerTitles">
           <span className="headerTitleSm">Welcome To</span>
-          <span className="headerTitleLg">E-com</span>
+          <span className="headerTitleLg">Ecom</span>
         </div>
         <img
           className="headerImg"
-          src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80"
+          src="https://images.unsplash.com/photo-1496171367470-9ed9a91ea931?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
           alt=""
         />
       </div>
